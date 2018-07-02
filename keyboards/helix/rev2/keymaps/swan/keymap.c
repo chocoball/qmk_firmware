d8892d287000f50ac3fb13663d7478ee205baa04
swanmatch <swan_match@yahoo.co.jp>
2018-07-03 08:07:34 +0900

    update serial.c

--- keyboards/helix/rev2/keymaps/swan/keymap.c
+++ keyboards/helix/rev2/keymaps/swan/keymap.c
@@ -74,35 +74,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Ctr  |  OS  | F12  |  F5  | Alt  | Bksp |Space |Enter | Del  |   -  |   \  |ZenHan| Menu |Right |
+   * | Ctr  |  OS  | F12  |  F5  | Alt  | Bksp |Space |Enter | Del  |   -  |   \  |ZenHan| Menu | Set  |
    * `-------------------------------------------------------------------------------------------------'
    */
-  [_QWERTY] = KEYMAP( \
-    KC_GESC, KC_1,    KC_2,   KC_3,          KC_4,          KC_5,                                   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,    \
-    KC_TAB,  KC_Q,    KC_W,   KC_E,          KC_R,          KC_T,                                   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,   \
-    TT(4),   KC_A,    KC_S,   KC_D,          KC_F,          KC_G,                                   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_COLN,   \
-    KC_LSFT, KC_Z,    KC_X,   KC_C,          KC_V,          KC_B,    KC_LBRC,       KC_RBRC,        KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,   \
-    KC_LCTL, KC_LGUI, KC_F12, LALT_T(KC_F5), RALT_T(KC_F5), KC_BSPC, LT(3, KC_SPC), RCTL_T(KC_ENT), KC_DEL, KC_MINS, KC_JYEN, KC_ZKHK, KC_APP,  TT(ADJUST) \
+  [_QWERTY] = LAYOUT( \
+    KC_GRV,    KC_1,    KC_2,   KC_3,          KC_4,          KC_5,                                   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,    \
+    KC_TAB,    KC_Q,    KC_W,   KC_E,          KC_R,          KC_T,                                   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,   \
+    TT(RAISE), KC_A,    KC_S,   KC_D,          KC_F,          KC_G,                                   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_COLN,   \
+    KC_LSFT,   KC_Z,    KC_X,   KC_C,          KC_V,          KC_B,    KC_LBRC,       KC_RBRC,        KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,   \
+    KC_LCTL,   KC_LGUI, KC_F12, LALT_T(KC_F5), RALT_T(KC_F5), KC_BSPC, LT(3, KC_SPC), RCTL_T(KC_ENT), KC_DEL, KC_MINS, KC_JYEN, KC_ZKHK, KC_APP,  TT(ADJUST) \
   ),
 
-  [_COLEMAK] = KEYMAP( \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
+  [_COLEMAK] = LAYOUT( \
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
   ),
-  [_DVORAK] = KEYMAP( \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
+  [_DVORAK] = LAYOUT( \
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
   ),
 
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  | F10  |PrtSc |
+   * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  | F10  |PrtSc |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |  Ins | ScLk | PgUp |Pouse |      |             |      |      |  Up  |      |      | F12  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
@@ -113,60 +113,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
-  [_LOWER] = KEYMAP( \
-    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                             KC_F6,   KC_F7,        KC_F8,        KC_F9,        KC_F10,  KC_PSCR, \
-    KC_TRNS, KC_INS,  KC_SLCK, KC_PGUP, KC_PAUS, KC_TRNS,                           KC_TRNS, KC_TRNS,      KC_UP,        KC_TRNS,      KC_TRNS, KC_F12,  \
-    TT(4),   KC_SLCT, KC_HOME, KC_PGDN, KC_END,  KC_TRNS,                           KC_TRNS, KC_LEFT,      KC_DOWN,      KC_RGHT,      KC_TRNS, KC_F11,  \
-    KC_TRNS, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_TRNS, KC_LBRC, KC_RBRC,         KC_TRNS, LCA(KC_LEFT), LCA(KC_DOWN), LCA(KC_RGHT), KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, QWERTY,  RCTL_T(KC_RPRN), KC_RCBR, KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS  \
+  [_LOWER] = LAYOUT( \
+    KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,        KC_F8,        KC_F9,        KC_F10,  KC_PSCR, \
+    _______,   KC_INS,  KC_SLCK, KC_PGUP, KC_PAUS, _______,                   _______, _______,      KC_UP,        _______,      _______, KC_F12,  \
+    TT(RAISE), KC_SLCT, KC_HOME, KC_PGDN, KC_END,  _______,                   _______, KC_LEFT,      KC_DOWN,      KC_RGHT,      _______, KC_F11,  \
+    _______,   KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______, _______, _______, _______, LCA(KC_LEFT), LCA(KC_DOWN), LCA(KC_RGHT), _______, _______, \
+    _______,   _______, _______, _______, _______, KC_LCBR, QWERTY,  _______, KC_RCBR, _______,      _______,      _______,      _______, _______  \
   ),
   /* Raise
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |NumLk |  /   |  *   |  -   |      |
-   * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |NumLk |  /   |  *   |  -   |      |
+   * |      |      |      |      |      |      |             |      |NumLk |  /   |  *   |  -   |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  Ins | ScLk | PgUp |Pouse |      |             |      |  7   |  8   |  9   |  +   |      |
+   * |      |      |RClick|MusUp |Lclick|      |             |      |  7   |  8   |  9   |  +   |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |Select| Home | PgDw | End  |      |             |      |  4   |  5   |  6   |  ,   |      |
+   * |      |      |MusLft|MusDw |MusRgt|      |             |      |  4   |  5   |  6   |  ,   |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      | Undo | Cut  | Copy |Paste |      |      |      |      |  1   |  2   |  3   |  =   |      |
+   * |      |      |      |      |      |      |      |      |      |  1   |  2   |  3   |  =   |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |      |  0   |  0   |  .   |Enter |      |
    * `-------------------------------------------------------------------------------------------------'
    */
-  [_RAISE] = KEYMAP( \
-    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_TRNS, \
-    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_7,    KC_8,    KC_9,    KC_PPLS, KC_TRNS, \
-    QWERTY,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_4,    KC_5,    KC_6,    KC_PCMM, KC_TRNS, \
-    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_PEQL, KC_TRNS, \
-    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TT(3),   KC_TRNS, KC_TRNS, KC_0,    KC_0,    KC_PDOT, KC_PENT, KC_TRNS  \
+  [_RAISE] = LAYOUT( \
+    _______,   _______, _______,    _______,    _______,     _______,                   _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, _______, \
+    _______,   _______, KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2,  _______,                   _______, KC_7,    KC_8,    KC_9,    KC_PPLS, _______, \
+    QWERTY,    _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______,                   _______, KC_4,    KC_5,    KC_6,    KC_PCMM, _______, \
+    _______,   _______, _______,    _______,    _______,     _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_PEQL, _______, \
+    _______,   _______, _______,    _______,    _______,     _______, LOWER,   _______, _______, KC_0,    KC_0,    KC_PDOT, KC_PENT, _______  \
   ),
 
   /* Adjust (Lower + Raise)
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |      |      |      |      |      |      |             |      |      |      |      |      |      |
+   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  | F10  |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |      |      |HueUp |      |      |             |      |      |ModeUp|      |      | F12  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |      |SadUp |HueDw |SadDw |      |             |      |BriDw |ModeDw|BriUp |      | F11  |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
+   * |      |      |      |      |      |      |      |      |      |SpdDw |      |SpdUp |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |LEDTgl|      |      |      |      |      |      |      |
+   * |      |      |      |      |      |      |      |LEDTgl|      |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
-  [_ADJUST] =  KEYMAP( \
-    RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, DEBUG,   \
-    KC_TRNS, KC_TRNS, KC_TRNS, RGB_HUI, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, RGB_MOD,  KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, RGB_SAD, RGB_HUD, RGB_SAI, KC_TRNS,                   KC_TRNS, RGB_VAD, RGB_RMOD, RGB_VAI, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, BL_TOGG, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS  \
+  [_ADJUST] =  LAYOUT( \
+    RESET,   _______, _______, _______, _______, _______,                   _______, _______, _______,  _______, _______, DEBUG,   \
+    _______, _______, _______, RGB_HUI, _______, _______,                   _______, _______, RGB_MOD,  _______, _______, _______, \
+    _______, _______, RGB_SAD, RGB_HUD, RGB_SAI, _______,                   _______, RGB_VAD, RGB_RMOD, RGB_VAI, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPD, _______,  RGB_SPI, _______, _______, \
+    _______, _______, _______, _______, _______, _______, BL_TOGG, RGB_TOG, _______, _______, _______,  _______, _______, _______  \
   )
 };
 #else
 #error "undefined keymaps"
 #endif
-
 
 #ifdef AUDIO_ENABLE
 
@@ -339,7 +337,6 @@ void matrix_init_user(void) {
     #endif
     //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
     #ifdef SSD1306OLED
-        TWI_Init(TWI_BIT_PRESCALE_1, TWI_BITLENGTH_FROM_FREQ(1, 800000));
         iota_gfx_init(!has_usb());   // turns on the display
     #endif
 }
