fe0bcb656e9c120bea9ce942168d0860afba0a6d
chocoball <shinji.sadato@gmail.com>
2019-02-02 20:31:25 +0900

    キーマップ修正

--- keyboards/colosseum44/rev1/keymaps/chocoball/keymap.c
+++ keyboards/colosseum44/rev1/keymaps/chocoball/keymap.c
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Adjust
      * ,-----------------------------------------.                ,------------------------------------------.
-     * |  Esc |  F1  |  F2  |  F3  |  F4  |  F5  |                |  F6  |  F7  |  F8  |  F9  | F10  |  F11  |
+     * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                |  F6  |  F7  |  F8  |  F9  | F10  |  F11  |
      * |------+------+--------+------+--------+--|                |------+------+------+------+------+-------|
      * |      |      |      |      |      |      |                | LTOG | LMOD |      | C A D| A Prt|  F12  |
      * |------+------+--------+------+--------+--|                |------+------+------+------+------+-------|
@@ -136,10 +136,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
   [_ADJUST] = LAYOUT(
-   KC_TRNS, KC_1,  KC_2,  KC_3,   KC_4,   KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_DEL,
-   KC_TRNS,KC_F1, KC_F2, KC_F3,  KC_F4,  KC_F5,       RGB_TOG, RGB_MOD,   KC_NO,LCA(KC_DEL), LALT(KC_PSCR),KC_PSCR,
-   KC_TRNS,KC_F6, KC_F7, KC_F8,  KC_F9, KC_F10,       RGB_VAD, RGB_VAI, RGB_HUD, RGB_HUI, RGB_SAD,  RGB_SAI,
-                     KC_TRNS,KC_TRNS,KC_RAISE, KC_TRNS,    KC_TRNS, KC_LOWER, KC_TRNS,KC_TRNS
+   KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,         KC_F6,   KC_F7,   KC_F8,      KC_F9,        KC_F10, KC_F11,
+   KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       RGB_TOG, RGB_MOD,   KC_NO,LCA(KC_DEL), LALT(KC_PSCR), KC_F12,
+   KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       RGB_VAD, RGB_VAI, RGB_HUD,    RGB_HUI,       RGB_SAD, RGB_SAI,
+              KC_TRNS,KC_TRNS,KC_LOWER, KC_TRNS,    KC_TRNS, KC_RAISE, KC_TRNS,KC_TRNS
    )
 };
 
