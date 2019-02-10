aa28f64ba3566b429c583be81c590bc48d37f135
chocoball <shinji.sadato@gmail.com>
2019-02-10 17:34:17 +0900

    keymap escと=の位置交換

--- keyboards/colosseum44/rev1/keymaps/chocoball/keymap.c
+++ keyboards/colosseum44/rev1/keymaps/chocoball/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    /* Qwerty
      * ,-----------------------------------------.                ,-------------------------------------------.
-     * |  Esc |   Q  |   W  |   E  |   R  |   T  |                |   Y  |   U  |   I  |   O  |   P  |    \   |
+     * |   =  |   Q  |   W  |   E  |   R  |   T  |                |   Y  |   U  |   I  |   O  |   P  |    \   |
      * |------+------+------+------+------+------|                |------+------+------+------+------+--------|
      * | Ctrl |   A  |   S  |   D  |   F  |   G  |                |   H  |   J  |   K  |   L  |   ;  | ' / Cmd|
      * |------+------+------+------+------+------|                |------+------+------+------+------+--------|
@@ -72,14 +72,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *               `-----------------------------------' `-----------------------------------'
      */
   [_QWERTY] = LAYOUT(
-        KC_ESC,  KC_Q, KC_W, KC_E, KC_R, KC_T,                    KC_Y, KC_U, KC_I,    KC_O,   KC_P,      KC_BSLS,
+        KC_EQL,  KC_Q, KC_W, KC_E, KC_R, KC_T,                    KC_Y, KC_U, KC_I,    KC_O,   KC_P,      KC_BSLS,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                    KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,   GUI_T(KC_QUOT),
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                    KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,   KC_RSFT,
           KC_LALT, KC_LGUI, LT(_LOWER, KC_SPC), KC_BSPC,      KC_TAB, LT(_RAISE, KC_ENT), KC_LALT, KC_NO
   ),
     /* Lower
      * ,-----------------------------------------.                ,------------------------------------------.
-     * |  =   |   1  |   2  |   3  |   4  |   5  |                |   6  |   7  |   8  |   9  |   0  |   -   |
+     * |  Esc |   1  |   2  |   3  |   4  |   5  |                |   6  |   7  |   8  |   9  |   0  |   -   |
      * |------+------+------+------+------+------|                |------+------+------+------+------+-------|
      * |      |   {  |   }  |   (  |   )  |   `  |                |      |      |      |      |      |   |   |
      * |------+------+------+------+------+------|                |------+------+------+------+------+-------|
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                      `----------------------------' `---------------------------'
      */
   [_LOWER] = LAYOUT(
-         KC_EQL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS,
+         KC_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TRNS, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_GRV,       KC_NO,  KC_NO, KC_NO,   KC_NO,   KC_TRNS, KC_PIPE,
         KC_TRNS, KC_NO,   KC_LBRC, KC_LBRC, KC_RBRC, KC_TILD,      KC_NO,  KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                          KC_TRNS,  KC_TRNS, KC_LOWER, KC_TRNS,     KC_TRNS, KC_RAISE, KC_TRNS, KC_TRNS
